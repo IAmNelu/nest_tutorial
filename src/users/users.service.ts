14b4844234158e2106a89db0f517eb94aec824da
Valentin Nelu <ifrimvale@gmail.com>
2021-08-03 15:55:12 +0200

    Body parsing, Dto, entities

---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [{ id: 0, name: 'Nelu' }];
+
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findById(uid: number): User {
+    return this.users.find(user => user.id === uid);
+  }
+
+  createUser(createUserDto: CreateUserDto): User {
+    const newUser = { id: Date.now(), ...createUserDto };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
